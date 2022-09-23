c3ec9cc43f923c516723d50090db98156d7fe76d
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-09-23 13:29:48 +0200

    First intial in String

---
+++ src/StringTests.java
@@ -0,0 +1,35 @@
+public class StringTests {
+
+    public static void main(String[]args) {
+        String randomString;
+        String myQuote;
+
+        randomString = "My name is Jeff";
+        myQuote= "He said: \"I am here \"";
+        System.out.println(randomString);
+        System.out.println(myQuote);
+
+        int n= 10;
+        String n1= "nndwdw";
+        String n2= "nndwdw";
+
+        System.out.println(randomString +". "+ myQuote);
+
+
+        if (n1.equals(n2)){
+            System.out.println("They are equal");
+        }
+        System.out.println(n1.length()); // length of the string
+        System.out.println(n1.charAt(2));
+        for (int i = 0; i < n1.length(); i++) {
+            System.out.println(n1.charAt(i));
+        }
+        System.out.println(n1.toLowerCase());
+        System.out.println(n1.toUpperCase());
+        System.out.println(n1.replace("n", "y"));
+
+        String letter= "abcde";
+        String moreLetter= "fghij";
+        System.out.println(letter.compareTo(moreLetter));
+    }
+}
